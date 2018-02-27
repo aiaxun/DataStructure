e3717534b7a768ccfda38c9905278567651f17c7
aiaxun <unSimple1993@163.com>
2018-02-27 14:46:50 +0800

    Basic BinaryTree
    添加基本二叉树实现代码

---
+++ BinaryTree.cpp
@@ -0,0 +1,57 @@
+#include "BinaryTree.h"
+
+#include <iostream>
+#include <stdlib.h>
+#include <malloc.h>
+
+using namespace std;
+
+BinaryTree::BinaryTree() {
+	createTree(root);
+}
+
+void BinaryTree::createTree(TreeNode *node){
+	char ch;
+	cin>>ch;
+	if (ch == '#') {
+		return;
+	} 
+	if (node == NULL){
+		node = (TreeNode *)malloc(sizeof(TreeNode));
+	}
+	if (node == NULL){
+		cout<<"Memory Malloc Failuer!"<<endl;
+		exit(-1);
+	}
+	node->val = ch;
+	createTree(node->left);
+	createTree(node->right);
+}
+
+void BinaryTree::inorderTreeWalk(TreeNode *node){
+	if (node!=NULL){
+		cout<<node->val<<" ";
+		inorderTreeWalk(node->left);
+		inorderTreeWalk(node->right);
+	} else {
+		return;
+	}
+}
+
+void BinaryTree::preorderTreeWalk(TreeNode *node){
+	if (node == NULL){
+		return ;
+	}
+	preorderTreeWalk(node->left);
+	cout<<node->val<<" ";
+	preorderTreeWalk(node->right);
+}
+
+void BinaryTree::postorderTreeWalk(TreeNode *node){
+	if (node == NULL){
+		return;
+	}
+	postorderTreeWalk(node->right);
+	cout<<node->val<<" ";
+	postorderTreeWalk(node->left);
+}
---
+++ BinaryTree.h
@@ -0,0 +1,23 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+typedef struct TreeNode {
+	char val;
+	struct TreeNode *left, *right;
+}TreeNode;
+
+class BinaryTree {
+private:
+	TreeNode *root;
+
+	void createTree(TreeNode *node);
+public:
+	BinaryTree();
+	~BinaryTree();
+
+	void inorderTreeWalk(TreeNode *node);
+
+	void preorderTreeWalk(TreeNode *node);
+
+	void postorderTreeWalk(TreeNode *node);
+}
